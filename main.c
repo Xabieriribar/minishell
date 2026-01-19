bf8ccd1900d35ca58479d968eb81fe23dd63760b
Rick <rspinellir13@gmail.com>
2026-01-19 11:17:06 +0100

    upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rick <rick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 11:56:33 by rick              #+#    #+#             */
-/*   Updated: 2026/01/18 17:32:42 by rick             ###   ########.fr       */
+/*   Updated: 2026/01/18 17:46:23 by rick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 int	main(void)
 {
-	test_init_list("< infile grep 'adios mundo' | cat hostname >> outfile");
+	//test_init_list("< infile grep 'adios mundo' | cat hostname >> outfile");
+	char **arr;
+	
+	arr = ft_split("hola como estas", ' ');
+	arr[2][1] = 'p';
 	return (0);
 }
