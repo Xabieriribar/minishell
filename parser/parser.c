26a11584f551b17c62e470ec8a178f74c94c1adb
Xabieriribar <xabieriribarrevuelta@gmail.com>
2026-01-20 11:59:31 +0100

    docs/add tests

---
+++ parser/parser.c
@@ -0,0 +1,16 @@
+#include "../includes/minishell.h"
+
+char **create_args(t_token **tokens)
+{
+    t_token *temp;
+    char **args;
+
+    temp = *tokens;
+    while (temp)
+    {
+        temp->value[i] =
+    }
+
+    return (args);
+
+}
