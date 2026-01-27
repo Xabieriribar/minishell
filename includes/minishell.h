b78234d09a126e203c3998d4a436dc7dc96008dc
Xabieriribar <xabieriribarrevuelta@gmail.com>
2026-01-27 18:46:29 +0100

    feat/add redirs tests

--- includes/minishell.h
+++ includes/minishell.h
@@ -87,5 +87,5 @@ void	sigint_handler(int sig);
 int test_grammar(int fd_grammar_tester);
 char *grammar_validator(t_token *head);
 int		ft_token_lstsize(t_token *lst);
-int		ft_is_redir(t_type *type);
+int		ft_is_redir(t_type type);
 #endif
