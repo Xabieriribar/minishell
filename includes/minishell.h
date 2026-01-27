3170c91ce081b1b0d2efbc3fdd1d900fdea2a4f1
Xabieriribar <xabieriribarrevuelta@gmail.com>
2026-01-27 09:41:25 +0100

    Manually restoring t_token prev pointer from main

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,9 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rick <rick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 18:38:42 by rick              #+#    #+#             */
-/*   Updated: 2026/01/23 16:31:57 by marvin           ###   ########.fr       */
 /*   Updated: 2026/01/25 14:06:49 by rick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -50,16 +49,12 @@ struct s_token
 	int		index;
 	int		dolar;
 	t_token	*next;
+	t_token	*prev;
 };
 
-// typedef struct s_node {
-// 	t_node_type node_type;
-// 	struct s_node *left_child;
-// 	struct s_node *right_child;
-// 	char **args;
-// 	// t_node *child ONLY FOR BONUS
-// 	t_redir *redir;
-// } t_node;
+typedef struct s_tree	t_tree;
+
+// ----------- TOKENIZER ---------- //
 
 void	lst_add_back_token(t_token **lst, t_token *new);
 t_token	*lstlast_token(t_token *lst);
@@ -89,13 +84,5 @@ void	test_init_list(char *line, char *expected);
 // ----------- SIGNALS ---------- //
 
 void	sigint_handler(int sig);
-
-
-// ----------- PARSER ---------- //
-
-int test_grammar(int fd_grammar_tester);
-int	ft_token_lstsize(t_token *lst);
-char *grammar_validator(t_token *head);
-int		ft_token_lstsize(t_token *lst);
 
 #endif
