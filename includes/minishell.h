ac5a32f0bc57ec1986ea52dbfa29dddff613e22f
Xabieriribar <xabieriribarrevuelta@gmail.com>
2026-02-08 11:21:03 +0100

    feat/change minishell.h for ast structure

--- includes/minishell.h
+++ includes/minishell.h
@@ -52,6 +52,24 @@ struct s_token
 	t_token		*prev;
 };
 
+typedef struct s_redirs
+{
+	t_type	redir_type;
+	char	*filename;
+	t_redirs	*next;
+}	t_redirs;
+
+typedef enum s_node_type
+{
+	COMMAND,
+	PIPE,
+} t_node_type;
+typedef struct s_node
+{
+	t_node_type	node_type;
+	char	**args;
+	t_redirs	*redirs;
+} s_node;
 typedef struct s_tree	t_tree;
 
 // ----------- TOKENIZER ---------- //
