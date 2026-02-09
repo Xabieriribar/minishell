1f8d00204829353ecb8ae53a415ca859cdf997ff
Xabieriribar <xabieriribarrevuelta@gmail.com>
2026-02-09 10:57:18 +0100

    feat/made tester work with double tokens

--- parser/create_tree.c
+++ parser/create_tree.c
@@ -1,26 +1,41 @@
 #include "minishell.h"
 
+int     create_multiple_args(t_node *node, t_token *token_list)
+{
+    int index;
+    index = 0;
+    if (!node || !token_list)
+        return 1;
+    while (token_list)
+    {
+        node->args[index] = token_list->value;
+        token_list = token_list->next;
+        index++;
+    }
+    return 0;
+}
+void    fill_tree(t_node *node)
+{
+    node->args[0] = NULL;
+    node->redirs= NULL;
+    node->left_child=NULL;
+    node->right_child=NULL;
+}
 t_node *init_tree(t_token *token_list)
 {
     t_node *node;
     int number_of_tokens;
 
     number_of_tokens = ft_token_lstsize(token_list);
-    node = ft_calloc(number_of_tokens, sizeof(struct s_node));
-    node->args = ft_calloc(number_of_tokens, sizeof(node->args));
-    if (!node->args)
+    node = malloc(sizeof(struct s_node) * number_of_tokens);
+    node->args = malloc(number_of_tokens * sizeof(node->args));
+    fill_tree(node);
+    if (!node->args || !node)
         perror("Malloc failed for the argument creation when size is one");
-    // node->args[0] = NULL;
-    // fill_tree_values(node);
-    if (!node)
-        perror("Malloc failed for the node creation");
-    // if (number_of_tokens == 1)
-    // {
-    //     node->args[0] = token_list->value;
-    //     node->args[1] = NULL;
-    //     node->redirs = NULL;
-    //     node->left_child = NULL;
-    //     node->right_child = NULL;
-    // }
+    if (number_of_tokens == 1)
+        node->args[0] = token_list->value;
+    else
+        if (create_multiple_args(node, token_list) != 0)
+            printf("Failed to create multiple args\n");
     return node;
 }
--- parser/test_trees.c
+++ parser/test_trees.c
@@ -1,9 +1,28 @@
 #include "minishell.h"
 
-int compare_args(t_node *tree, char *expected_args)
+int compare_args(t_node *tree, char *expected_args, int number_of_tokens)
 {
-    if (strcmp(tree->args[0], expected_args) != 0)
+    if (!tree->args[0])
         return 1;
+    if (number_of_tokens == 1)
+    {
+        if (strcmp(tree->args[0], expected_args) != 0)
+            return 1;
+    }
+    {
+        char **expected_arguments = ft_split(expected_args, ' ');
+        int i = 0;
+        while (expected_arguments[i])
+        {
+            if (strcmp(tree->args[i], expected_arguments[i]) != 0)
+            {
+                printf("Here is %s and %s", tree->args[i], expected_arguments[i]);
+                return 1;
+            }
+            i++;
+        }
+    }
+    printf("Here then?\n");
     return 0;
 }
 
@@ -42,22 +61,24 @@ int test_tree(int fd_tree_tester)
     int i = 1;
     char *line;
     t_token *head;
+    int number_of_tokens;
     t_node *tree;
     while ((line = get_next_line(fd_tree_tester)))
     {
         char *commands = strtok(line, "::");
         head = init_list(commands);
         tree = init_tree(head);
-        if (compare_args(tree, commands) != 0)
+        number_of_tokens = ft_token_lstsize(head);
+        if (compare_args(tree, commands, number_of_tokens) != 0)
             return(printf("%d %s[FAIL]%s\n", i, RED, RESET), 1);
-        // else
-        //     printf("%d %s[PASS]%s\n", i, GREEN, RESET);
+        else
+            printf("%d %s[PASS]%s\n", i, GREEN, RESET);
         // // if (compare_redirs(tree, head) != 0)
         // //     printf("Redirs creation failed\n");
         // // if (compare_childs(tree, head) != 0)
         // //     printf("Child creation failed\n" != 0);
-        // printf("ARGS: "), print_args(tree->args);
-        // i++;
+        i++;
+        printf("ARGS: "), print_args(tree->args);
     }
     return 0;
 }
